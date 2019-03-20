38c7d5810808c3040b63aaa93524da19977e3593
Ferawati Hartanti Pratiwi <ferawati.10109266@gmail.com>
2019-03-20 18:06:41 +0700

    fix bug

--- src/database/new_onboarding.go
+++ src/database/new_onboarding.go
@@ -1,21 +1,29 @@
 package database
 
 import (
+	"database/sql"
+
 	"github.com/bandung-telegram-bot-golang/src/helper"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func NewOnboarding(username string) bool {
 	var onboarding bool
+	var count int
 
 	db := DBConnection()
 
+	rowCount := db.QueryRow("SELECT on_flag from onboarding_member where on_username='@" + username + "'").Scan(&count)
 	row, err := db.Query("SELECT on_flag from onboarding_member where on_username='@" + username + "'")
 	helper.ErrorMessage(err)
 
-	for row.Next() {
-		err = row.Scan(&onboarding)
-		helper.ErrorMessage(err)
+	if rowCount == sql.ErrNoRows {
+		return true
+	} else {
+		for row.Next() {
+			err = row.Scan(&onboarding)
+			helper.ErrorMessage(err)
+		}
 	}
 
 	return onboarding
