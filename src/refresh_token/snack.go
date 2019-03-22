c01dcbfcde5e50ee3ef075a8e013b79e14ba5ea2
Ferawati Hartanti Pratiwi <ferawati.10109266@gmail.com>
2019-03-22 11:39:59 +0700

    add refresh token

---
+++ src/refresh_token/snack.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"os"
+	"time"
+
+	"github.com/bandung-telegram-bot-golang/src/helper"
+	"github.com/joho/godotenv"
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
+func main() {
+	env := godotenv.Load()
+	helper.ErrorMessage(env)
+	b, err := tb.NewBot(tb.Settings{
+		Token:  os.Getenv("TOKEN_SNACK"),
+		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
+	})
+	helper.ErrorMessage(err)
+
+	b.Handle("/hello", func(m *tb.Message) {
+		b.Send(m.Sender, "hello world")
+	})
+
+	b.Start()
+}
